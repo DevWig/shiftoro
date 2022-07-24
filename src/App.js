789a1f2b793fb7b67094d78b1921884df55ad38d
Austin Wiginton <ahwig93@gmail.com>
2022-07-23 23:44:09 -0500

    Overhaul app.css file with new custom styles, import all necessary components and build out routing for app.js, and wrap application with FirebaseProvider for state management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Schedule from './pages/Schedule'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Navbar from './components/Navbar'
+import Help from './pages/Help'
+
+import {FirebaseProvider} from './context/FirebaseContext'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <FirebaseProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/profile' element={<Profile/>} />
+          <Route path='/help' element={<Help/>} />
+          <Route path='/schedule' element={<Schedule/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          
+        </Routes>
+        
+      </Router>
+    </FirebaseProvider>
   );
 }
 
