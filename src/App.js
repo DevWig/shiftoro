2f46d2603324fa1c6527b5260b8a79da1ffdebdd
Austin Wiginton <ahwig93@gmail.com>
2022-07-31 22:40:12 -0500

    Create new page for creating new staff accounts and add new page and forgot password page routes to app.js file

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Profile from './pages/Profile'
 import Schedule from './pages/Schedule'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import CreateStaffAccount from './pages/CreateStaffAccount'
 import Navbar from './components/Navbar'
 import Help from './pages/Help'
+import ForgotPassword from './pages/ForgotPassword'
 
 import {FirebaseProvider} from './context/FirebaseContext'
 
@@ -20,9 +22,11 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/profile' element={<Profile/>} />
           <Route path='/help' element={<Help/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
           <Route path='/schedule' element={<Schedule/>} />
           <Route path='/sign-in' element={<SignIn/>} />
           <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/create-staff-account' element={<CreateStaffAccount/>} />
           
         </Routes>
         
---
+++ src/pages/CreateStaffAccount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CreateStaffAccount() {
+  return (
+    <>
+      <div>Create Staff Account</div>
+    </>
+    
+  )
+}
+
+export default CreateStaffAccount
